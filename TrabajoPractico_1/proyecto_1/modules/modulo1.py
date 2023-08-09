a5e4f61f894a4c21f653a2020ec2caf19c95d447
aguswiesner <wiesneragustina@gmail.com>
2023-08-09 12:44:54 -0300

    función de opción 1

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,1 +1,11 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
+lista=[]
+lisa_sin=[]
+def mostrar_lista_peliculas (archivo_peliculas):
+    """Función que lee el archivo con las frases de las peliculas y muestra una lista de tuplas"""
+    with open (archivo_peliculas, "r",encoding="utf-8") as f: #reconoce caracteres especiales en la lista
+        for linea in f:
+            frase,pelicula=linea.rstrip("\n").split(";")
+            lista.append(pelicula)
+    lista_sin=set(lista)
+    return [(i+1,elemento) for i,elemento in enumerate (sorted(lista_sin))] #elemento no es variable, es como i, en el for
