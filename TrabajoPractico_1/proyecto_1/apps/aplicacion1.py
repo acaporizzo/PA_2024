4f2a0aacb36df77c0ef100c7e6a7115ed66f1b60
Ana Clara <anaclaritapolari@gmail.com>
2023-09-06 01:43:21 -0300

    comentarios listos

--- TrabajoPractico_1/proyecto_1/apps/aplicacion1.py
+++ TrabajoPractico_1/proyecto_1/apps/aplicacion1.py
@@ -18,44 +18,53 @@ Elige una opción
 RUTA="./data/"
 DIRECCION=RUTA + "frases_de_peliculas.txt"
 DIRECCION1=RUTA+"registro de opciones selecionadas.txt"
+
 lista_opciones=[]
 opcion=int(input("Ingrese una opción: "))
+
 guardar_opciones(opcion)
 with open (DIRECCION, "r",encoding="utf-8") as f:
       lista1=f.readlines()
       frases_y_pelis=[(linea.strip().split(';')[0], linea.strip().split(';')[1]) for linea in lista1]
-while opcion!=5:
+
+while opcion!=5: #si la opción es 5, sale del bucle y finaliza el código
+
       if opcion == 1:
-            for i in mostrar_lista_peliculas(lista1):
+            for i in mostrar_lista_peliculas(lista1): 
                   print(str(i[0])+")",i[1])
-      elif opcion==2:
+
+      elif opcion == 2:
             repeticiones=int(input("Ingrese la cantidad de frases en la que consistirá la trivia (mínimo 3, máximo 10): "))
             while repeticiones<3 or repeticiones>10:
                   repeticiones=int(input("Ingrese nuevamente la cantidad. Debe ser un número entre 3 y 10: \n"))
             for i in range(1,repeticiones+1):
-                  lista=trivia(frases_y_pelis)  #va a ser una lista con los tres datos.
+                  lista=trivia(frases_y_pelis)  #va a ser una lista con los tres datos: la frase, la opción ganadora y una tupla con las 3 opciones a mostrar
                   print("\nAdivine a que película pertenece la siguiente frase: \n","\n"+str(i)+")",lista[0])
                   print("\nLas opciones para elegir son: ")
                   for i,j in zip(lista[2],range(1,4)): #agrega numero a las opciones de peliculas
                         print(str(j)+")",i)        
                   opcion=int(input("\nElija la opción correcta, ingresando 1, 2 o 3 : "))
                   if opcion-1==lista[2].index(lista[1]):
-                        print("\n¡¡¡Felicitaciones, la opción elegida es la correcta!!!\n")
+                        print("\n¡¡¡Felicitaciones, la opción elegida es la correcta!!!\n")   #compara resultados
                   elif opcion-1!=lista[2].index(lista[1]):
                         print("\nLa opción es incorrecta, la opción correcta es:",lista[1])
                   else:
                         print("El número ingresado no es una opción posible")
             
-      elif opcion==3:
+      elif opcion == 3:
             print("El historial es: ")
-            for i in mostrar_opciones_seleccionadas(DIRECCION1):
+            for i in mostrar_opciones_seleccionadas(DIRECCION1): #ruta de archivo txt con los datos solicitados
                   print(i)
-      elif opcion==4:
+
+      elif opcion == 4:
             borrar_opciones(DIRECCION1)
             print("Se eliminó correctamente el historial.")
-      elif opcion>5:
+
+      elif opcion > 5:
             print("La opción elegida no es correcta, vuelva a intentarlo.")
+            
       opcion=int(input("\nIngrese otra opción: "))
       guardar_opciones(opcion)
+
 if opcion==5:
       print("Gracias por utilizar el programa.")
--- TrabajoPractico_1/proyecto_1/server.py
+++ TrabajoPractico_1/proyecto_1/server.py
@@ -4,8 +4,8 @@ import datetime
 aciertos=0
 app = Flask("server")
 contador_repeticiones=0 
-info = False 
-guardado = False 
+info = False      #inicia falsa. Mientras no cambie su valor, no hay resultados.
+guardado = False      #inicia falsa, significa que no hay resultados para mostrar porque la trivia aún no se jugó
 lista=[]
 nombre_de_usuario = "" 
 numero_de_opciones = 3 
@@ -15,64 +15,74 @@ resultados_partidas=[]
 RUTA="./data/"
 DIRECCION=RUTA + "frases_de_peliculas.txt"
 
-with open (DIRECCION, "r",encoding="utf-8") as f:
+with open (DIRECCION, "r",encoding="utf-8") as f:  #archivo con frases y películas por remglón
     lista1=f.readlines()
-    frases_y_pelis=[(linea.strip().split(';')[0], linea.strip().split(';')[1]) for linea in lista1]
-with open("./data/resultados_historicos.txt", "w"):
+    frases_y_pelis=[(linea.strip().split(';')[0], linea.strip().split(';')[1]) for linea in lista1] #lista donde cada elemento es una línea del archivo
+
+with open("./data/resultados_historicos.txt", "w"):  #archivo nuevo, para escribir resultados
         pass
 
 @app.route("/",methods=["GET", "POST"]) 
 def home():
     global aciertos, contador_repeticiones, fecha_hora, guardado, info, mensaje, nombre_de_usuario, numero_de_opciones
     mensaje = "El número de opciones debe estar entre 3 y 10."
+
     if request.method == 'POST':
         numero_de_opciones=int(request.form['input_numero'])
         nombre_de_usuario=request.form['input_nombre']
 
         if numero_de_opciones >= 3 and numero_de_opciones <= 10: 
-            guardado = False
-            fecha_hora = datetime.datetime.now().strftime('%d/%m/%y %H:%M')
+            guardado = False   
+            #que guardado sea False implica que deben guardarse los datos.
+            fecha_hora = datetime.datetime.now().strftime('%d/%m/%y %H:%M') 
+            #se define al INICIO de la jugada
             return redirect(url_for('jugar_trivia'))
         else:
-            numero_de_opciones = 0
+            numero_de_opciones = 0  
             return render_template("home.html", mensaje=mensaje, numero_de_opciones=numero_de_opciones)
     
     aciertos = 0
-    contador_repeticiones = 0
-    info=False
+    contador_repeticiones = 0      #se reinician valores
+    info=False  
+
     return render_template("home.html", mensaje=mensaje, numero_de_opciones=numero_de_opciones)
 
 @app.route("/trivia", methods=["GET", "POST"])
 def jugar_trivia():
     global contador_repeticiones, frases_y_pelis, guardado, lista, numero_de_opciones
 
-    if contador_repeticiones <= numero_de_opciones:
+    if contador_repeticiones <= numero_de_opciones:   #durante la trivia
         lista = trivia(frases_y_pelis)
         contador_repeticiones+=1
         return render_template("trivia.html",lista=lista)
     else:
-        guardar_datos_del_juego(nombre_de_usuario, calificacion, fecha_hora)
-        guardado = True
+        guardar_datos_del_juego(nombre_de_usuario, calificacion, fecha_hora)   
+        #si no se cumple la condición, significa que se terminó de jugar la trivia y se deben guardar los datos.
+        guardado = True    
+        #significa que los datos de esa jugada ya fueron guardados
         return render_template("home.html",numero_de_opciones=numero_de_opciones,nombre_de_usuario=nombre_de_usuario)
+    
 @app.route("/respuestas", methods=["GET", "POST"])
 def respuestas():
     global aciertos, calificacion, guardado, numero_de_opciones, opcion_elegida, respuesta
-    
 
     if request.method == 'POST':
         opcion_elegida = request.form['opcion_elegida']
 
-    if opcion_elegida == lista[1]:
+    if opcion_elegida == lista[1]: 
+        #se compara lo seleccionado por el usuario y la opción correcta, segundo elemento de lista
         aciertos+=1
         calificacion=(f"Su calificación es: {aciertos}/{numero_de_opciones}")
         respuesta="¡Correcta!"  
-
     else:
+        #si no se cumple, no se suman aciertos
         calificacion=(f"Su calificación es: {aciertos}/{numero_de_opciones}")
         respuesta=(f"¡Incorrecta!, la respuesta correcta es: {lista[1]}.")
 
-    if contador_repeticiones==numero_de_opciones:
+    if contador_repeticiones == numero_de_opciones:
         guardar_datos_del_juego(nombre_de_usuario, calificacion, fecha_hora)
+        #se guardan los datos cuando se finaliza la trivia, no durante.
+
     return render_template("respuestas.html", respuesta=respuesta,calificacion=calificacion, contador_repeticiones=contador_repeticiones, numero_de_opciones=numero_de_opciones)
 
 @app.route("/resultados", methods=["GET", "POST"])
@@ -82,11 +92,13 @@ def ver_resultados():
 
     try: 
         with open ("./data/resultados_historicos.txt", "r") as f:
-            resultados_partidas=f.readlines()
+            resultados_partidas=f.readlines() 
+            #lista con resultados de partidas anteriores
             if not resultados_partidas:
                 info = True
                 advertencia = "No hay resultados para mostrar ya que todavía no empezó la trivia"
     except FileNotFoundError: 
+        #excepción en el caso de no encontrar el archivo, ya sea por estar vacío o por otro problema
         info = True
         
     return render_template("resultados.html", resultados_partidas=resultados_partidas, advertencia=advertencia, info=info)  
