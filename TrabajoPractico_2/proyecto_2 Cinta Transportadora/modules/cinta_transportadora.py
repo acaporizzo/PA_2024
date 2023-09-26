27e4164fe89493369ad3ec9bd26e2d5613852df7
aguswiesner <wiesneragustina@gmail.com>
2023-09-26 12:05:20 -0300

    terminados los docstrigns de la clase de Cinta_Transportadora

--- TrabajoPractico_2/proyecto_2 Cinta Transportadora/modules/cinta_transportadora.py
+++ TrabajoPractico_2/proyecto_2 Cinta Transportadora/modules/cinta_transportadora.py
@@ -3,25 +3,32 @@ from modules.detector_alimentos import DetectorAlimento
 
 class Cinta_Transportadora:
 
-    def transportar(self, n):
+    def transportar(self, p_n_alimentos):
+        """Método que va a simular el transporte de los alimentos, donde en cada iteración del while se van a 
+        determinar n alimentos especificando su nombre (kiwi, manzana, papa, zanahoria o undefined) y peso. 
+        Luego cada alimento se agrega a la lista de alimentos. 
+
+        Args:
+            p_n_alimentos (int): determina cuantos alimentos van a pasar por la cinta
+        """
         lista_de_alimentos= []
-        detector= DetectorAlimento()
+        detector= DetectorAlimento() #instanciamos un dectector de alimentos 
 
-        while len(lista_de_alimentos) < n:            
-            diccionario= detector.detectar_alimento()
-            nombre = diccionario['alimento']
-            peso = diccionario['peso']
+        while len(lista_de_alimentos) < p_n_alimentos:            
+            datos_alimento = detector.detectar_alimento()
+            nombre_alimento = datos_alimento['alimento']
+            peso_alimento = datos_alimento['peso']
 
-            if nombre == "undefined":
+            if nombre_alimento == "undefined":
                 continue
-            if nombre == "kiwi":
-                alimento= Kiwi(nombre, peso)
-            elif nombre == "manzana":
-                alimento= Manzana(nombre, peso)
-            elif nombre  == "papa":
-                alimento= Papa(nombre, peso)
-            elif nombre == "zanahoria":
-                alimento= Zanahoria(nombre, peso)
+            if nombre_alimento == "kiwi":
+                alimento= Kiwi(nombre_alimento, peso_alimento)
+            elif nombre_alimento == "manzana":
+                alimento= Manzana(nombre_alimento, peso_alimento)
+            elif nombre_alimento  == "papa":
+                alimento= Papa(nombre_alimento, peso_alimento)
+            elif nombre_alimento == "zanahoria":
+                alimento= Zanahoria(nombre_alimento, peso_alimento)
             lista_de_alimentos.append(alimento)  
             
         return(lista_de_alimentos)
