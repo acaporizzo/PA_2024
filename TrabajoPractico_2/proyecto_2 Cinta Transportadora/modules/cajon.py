c5b7ddd980ad59a95ecb17943801f5792a76cd35
aguswiesner <wiesneragustina@gmail.com>
2023-09-26 11:14:42 -0300

    terminamos las clases Alimentos y Cajon con docstringsincuidos

--- TrabajoPractico_2/proyecto_2 Cinta Transportadora/modules/cajon.py
+++ TrabajoPractico_2/proyecto_2 Cinta Transportadora/modules/cajon.py
@@ -1,17 +1,16 @@
 from modules.alimentos import Kiwi, Manzana, Papa, Zanahoria
 
-class Cajon_Alimentos:
+class Cajon:
     def __init__(self, p_lista_de_alimentos):
         self._lista_de_alimentos= p_lista_de_alimentos
 
-    def calcular_aw_prom(self, p_lista_aw):
-        if not p_lista_aw:  
-            return(0)
-        promedio= sum(p_lista_aw)/len(p_lista_aw)
-        promedio= round(promedio,2)
-        return(promedio) 
-    
     def agregar_y_calcular_aw(self, p_lista_de_alimentos):
+        """método que calcula la actividad acuosa de cada alimento que pasa por la cinta transportadora y lo agrega a la lista del
+        alimento correspondiente 
+
+        Args:
+            p_lista_de_alimentos (lista): todos los alimentos que pasan por la cinta transportadora
+        """
         aw_kiwis= []
         aw_manzanas= []
         aw_papas= []
@@ -28,3 +27,15 @@ class Cajon_Alimentos:
             if isinstance(alimento, Zanahoria):
                 aw_zanahorias.append(aw_de_alimento)
         return(aw_kiwis, aw_manzanas, aw_papas, aw_zanahorias)
+
+    def calcular_aw_prom(self, p_lista_aw):
+        """método que calcula la actividad acuosa promedio total de cada tipo de alimento que pasa por la cinta transportadora  
+        y lo divide por la cantidad de elementos que se encuentran en la lista y luego redondea al número con dos decimales
+        Args:
+            p_lista_aw (lista): actividad acuosa de cada alimento siendo todos del mismo tipo
+        """
+        if not p_lista_aw:  
+            return(0)
+        promedio= sum(p_lista_aw)/len(p_lista_aw)
+        promedio= round(promedio,2)
+        return(promedio) 
