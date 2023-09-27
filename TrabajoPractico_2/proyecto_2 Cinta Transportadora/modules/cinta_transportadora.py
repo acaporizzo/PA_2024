d9bcb9cd34697d2793c32fbfb251a1eee6551f64
aguswiesner <wiesneragustina@gmail.com>
2023-09-27 10:43:06 -0300

    medificamos cinta transportadora

--- TrabajoPractico_2/proyecto_2 Cinta Transportadora/modules/cinta_transportadora.py
+++ TrabajoPractico_2/proyecto_2 Cinta Transportadora/modules/cinta_transportadora.py
@@ -10,31 +10,24 @@ class Cinta_Transportadora:
         Args:
             p_n_alimentos (int): determina cuantos alimentos van a pasar por la cinta
         """
-        contador_alimentos = 0
         detector= DetectorAlimento() #instanciamos un detector de alimentos 
         lista_de_alimentos= []
        
-        while contador_alimentos < p_n_alimentos:      
+        while len(lista_de_alimentos) < p_n_alimentos:      
             datos_alimento = detector.detectar_alimento()
             tipo_alimento = datos_alimento['alimento']
             peso_alimento = datos_alimento['peso']
 
             if tipo_alimento == "undefined":
-                contador_alimentos += 1
                 continue
             elif tipo_alimento == "kiwi":
                 alimento= Kiwi(peso_alimento)
-                contador_alimentos += 1
             elif tipo_alimento == "manzana":
                 alimento= Manzana(peso_alimento)
-                contador_alimentos += 1
             elif tipo_alimento  == "papa":
                 alimento= Papa(peso_alimento)
-                contador_alimentos += 1
             elif tipo_alimento == "zanahoria":
                 alimento= Zanahoria(peso_alimento)
-                contador_alimentos += 1
-
             lista_de_alimentos.append(alimento)  
             
         return(lista_de_alimentos)
--- TrabajoPractico_2/proyecto_2 Cinta Transportadora/tests/test_modulo1.py
+++ TrabajoPractico_2/proyecto_2 Cinta Transportadora/tests/test_modulo1.py
@@ -1,42 +1,77 @@
 # Archivo de test para realizar pruebas unitarias del modulo1
 import unittest
 from modules.alimentos import Kiwi, Manzana, Papa, Zanahoria
-from modules.cinta_transportadora import clasificar_alimentos
-from modules.cajon import agregar_y_calcular_aw, calcular_aw_prom_diccionario, calcular_aw_prom
+from modules.cinta_transportadora import Cinta_Transportadora
+from modules.cajon import Cajon
 
 class TestAlimentos(unittest.TestCase):
 
     def test_kiwi_calcular_aw(self):
-        kiwi = Kiwi(0.1)  # Peso del kiwi
-        result = kiwi.calcular_aw()
-        self.assertAlmostEqual(result, 0.3902285416, delta=1e-6)  # Ajusta el valor esperado según tu implementación
+        kiwi = Kiwi(0.07)  # instanciamos un kiwi
+        resultado_de_aw = kiwi.calcular_aw()
+        self.assertAlmostEqual(resultado_de_aw, 0.5357, places=3) 
 
     def test_manzana_calcular_aw(self):
-        manzana = Manzana(0.2)  # Peso de la manzana
-        result = manzana.calcular_aw()
-        self.assertAlmostEqual(result, 0.3548387096, delta=1e-6)  # Ajusta el valor esperado según tu implementación
+        manzana = Manzana(0.18)  # instanciamos una manzana
+        resultado_de_aw = manzana.calcular_aw()
+        self.assertAlmostEqual(resultado_de_aw, 0.853, places=3)
 
     def test_papa_calcular_aw(self):
-        papa = Papa(0.3)  # Peso de la papa
-        result = papa.calcular_aw()
-        self.assertAlmostEqual(result, 0.5396284552, delta=1e-6)  # Ajusta el valor esperado según tu implementación
+        papa = Papa(0.15)  # instanciamos una papa
+        resultado_de_aw = papa.calcular_aw()
+        self.assertAlmostEqual(resultado_de_aw, 0.803, places=3) 
 
     def test_zanahoria_calcular_aw(self):
-        zanahoria = Zanahoria(0.4)  # Peso de la zanahoria
-        result = zanahoria.calcular_aw()
-        self.assertAlmostEqual(result, 0.783473703, delta=1e-6)  # Ajusta el valor esperado según tu implementación
+        zanahoria = Zanahoria(0.175)  # instanciamos una zanahoria
+        resultado_de_aw = zanahoria.calcular_aw()
+        self.assertAlmostEqual(resultado_de_aw, 0.793, places=3)  
+
+class TestCintaTransportadora(unittest.TestCase):
+
+    def test_clasificar_alimentos(self):
+    
+        cinta_transportadora = Cinta_Transportadora()
+        n_alimentos = 5  
+        lista_de_alimentos = cinta_transportadora.clasificar_alimentos(n_alimentos)
+
+        self.assertEqual(len(lista_de_alimentos), n_alimentos)
+
+        for alimento in lista_de_alimentos:
+            
+            self.assertTrue(isinstance(alimento, (Kiwi, Manzana, Papa, Zanahoria)))
+
+class TestCajon (unittest.TestCase):
+
+    def test_agregar_y_calcular_aw (self):
+
+        alimentos = [
+            Kiwi(0.07),
+            Manzana(0.18),
+            Papa(0.15),
+            Zanahoria(0.175),
+        ]    
+        cajon_de_n_alimentos = Cajon(alimentos)
+        diccionario_con_listas_aw = cajon_de_n_alimentos.agregar_y_calcular_aw(alimentos)
+
+        self.assertTrue('Kiwi' in diccionario_con_listas_aw)
+        self.assertTrue('Manzana' in diccionario_con_listas_aw)
+        self.assertTrue('Papa' in diccionario_con_listas_aw)
+        self.assertTrue('Zanahoria' in diccionario_con_listas_aw)
+
+    def test_calcular_aw_prom_diccionario(self):
+        diccionario_aw = {
+            'Kiwi': [0.9, 0.85, 0.92],
+            'Manzana': [0.88, 0.91, 0.87],
+            'Papa': [0.78, 0.82],
+            'Zanahoria': [0.75, 0.79, 0.77],
+        }
+    
+        cajon_de_n_alimentos = Cajon(diccionario_aw)
+    #def test_calcular_aw_prom(self)
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
-    
-class Test_ClasificarAlimentos(unittest.TestCase):
-
-
-class Test_Agregar_y_Calcular_aw(unittest.TestCase):
-
-
-class Test_Calcular_aw_Prom_Diccionario(unittest.TestCase):
-
-
-class Test_Calcular_aw_prom(unittest.TestCase):
-
